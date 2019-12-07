fb5c2ea403b1485deebb3fd16b953c2ac8cdd05f
MauRamos334455 <mauri334455@comunidad.unam.mx>
2019-12-07 15:31:04 -0600

    Create s-05-secuencias.sql

---
+++ s-05-secuencias.sql
@@ -0,0 +1,156 @@
+--@Autor(es): Carolina Kennedy Villa
+--            Ricardo Garcia Garcia
+--           César Mauricio Ramos Villaseñor
+--@Fecha creación: 07/12/2019
+--@Descripción: Creación de secuencias (DDL)
+--de la base de datos para el proyecto GlobalHome.
+
+create sequence usuario_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence vivienda_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence contrato_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence alquiler_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence imagen_seq
+    start with 1
+    increment by 1
+    minvalue 1
+    maxvalue 20
+    nocycle
+    cache 20
+    order
+;
+
+create sequence clabe_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence notificacion_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence mensaje_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence tarjeta_credito_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence transaccion_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence servicio_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence vivienda_servicio_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence status_vivienda_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence historico_vivienda_seq
+    start with 1
+    increment by 1
+    nominvalue
+    nomaxvalue
+    nocycle
+    cache 20
+    order
+;
+
+create sequence mensualidad_seq
+    start with 1
+    increment by 1
+    minvalue  1
+    maxvalue 240
+    nocycle
+    cache 20
+    order
+;
