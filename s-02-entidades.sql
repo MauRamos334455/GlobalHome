4a146e8ede329c56b50823237526b46050407532
MauRamos334455 <mauri334455@comunidad.unam.mx>
2019-12-07 12:49:28 -0600

    Llaves foraneas
    Llaves foraneas en DDL y ultimos cambios en el modelo logico

--- s-02-entidades.sql
+++ s-02-entidades.sql
@@ -18,8 +18,6 @@ create table usuario(
     apellido_materno varchar2(40),
     contrasenia varchar2(15) not null,
 
-
-
 );
 
 prompt TABLA VIVIENDA
@@ -29,7 +27,7 @@ create table vivienda(
     longitud varchar2(40) not null,
     latitud varchar2(40) not null,
     direccion varchar2(100) not null,
-    capacidad number(10,0) not null,
+    capacidad number(4,0) not null,
     descripcion varchar2(2000) not null,
     fecha_status date not null,
     es_renta number(1,0) not null,
@@ -54,7 +52,7 @@ create table vivienda_renta(
 );
 
 prompt TABLA VIVIENDA_VENTA
-create table venta(
+create table vivienda_venta(
     vivienda_id number(10,0)
       constraint vivienda_venta_pk primary key,
     numero_catastral number(20,0) not null,
@@ -67,7 +65,7 @@ create table venta(
 );
 
 prompt TABLA VIVIENDA_VACACION
-create table vacacion(
+create table vivienda_vacacion(
     vivienda_id number(10,0)
       constraint vivienda_vacacion_pk primary key,
     fecha_inicio date not null,
@@ -100,6 +98,14 @@ create table alquiler(
     alquiler_id number(10,0)
       constraint alquiler_pk primary key,
     folio varchar2(20) not null,
+    usuario_id number(10,0) not null,
+    vivienda_id number(10,0) not null,
+    constraint alquiler_usuario_id_fk
+      foreign key (usuario_id)
+      references usuario(usuario_id),
+    constraint alquiler_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda(vivienda_id),
 
 );
 
@@ -108,16 +114,22 @@ create table imagen(
     vivienda_id number(10,0)
     numero number(2,0),
     contenido blob not null,
-    constraint imagen_pk primary key (vivienda_id, numero),
-
+    constraint imagen_pk
+      primary key (vivienda_id, numero),
+    constraint vivienda_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda(vivienda_id),
 );
 
 prompt TABLA CLABE
 create table  clabe(
     clabe_id number(10,0)
       constraint clabe_pk primary key,
-    clabe varchar2(40) not null,
-
+    clabe number(18,0) not null,
+    vivienda_id number(10,0) not null,
+    constraint clabe_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda_renta(vivienda_id),
 );
 
 prompt TABLA NOTIFICACION
@@ -125,7 +137,15 @@ create table notificacion(
     notificacion_id number(10,0)
       constraint notificacion_pk primary key,
     numero_celular number(15,0) not null,
-
+    enviado number(1,0) not null,
+    usuario_id number(10,0) not null,
+    vivienda_id number(10,0) not null,
+    constraint notificacion_usuario_id_fk
+      foreign key (usuario_id)
+      references usuario(usuario_id),
+    constraint notificacion_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda_vacacion(vivienda_id),
 );
 
 prompt TABLA MENSAJE
@@ -134,7 +154,18 @@ create table mensaje(
       constraint mensaje_pk primary key,
     contenido varchar2(2000) not null,
     visto number(1,0) not null,
-
+    respuesta_id number(10,0),
+    usuario_id number(10,0) not null,
+    vivienda_id number(10,0) not null,
+    constraint mensaje_respuesta_id_fk
+      foreign key (respuesta_id)
+      references mensaje(respuesta_id),
+    constraint mensaje_usuario_id_fk
+      foreign key (usuario_id)
+      references usuario(usuario_id),
+    constraint mensaje_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda(vivienda_id),
 );
 
 prompt TABLA TARJETA_CREDITO
@@ -144,7 +175,10 @@ create table tarjeta_credito(
     numero_tarjeta number(16,0) not null,
     mes_expiracion number(2,0) not null,
     anio_expiracion number(2,0) not null,
-
+    usuario_id number(10,0) not null,
+    constraint tc_usuario_id_fk
+      foreign key (usuario_id)
+      references usuario(usuario_id),
 );
 
 prompt TABLA TRANSACCION
@@ -152,8 +186,15 @@ create table transaccion (
     transaccion_id number(10,0)
       constraint transaccion_pk primary key,
     comision number(24,4) not null,
-    clave_interbancaria varchar2(40) not null,
-
+    clave_interbancaria number(18,0) not null,
+    usuario_id number(10,0) not null,
+    vivienda_id number(10,0) not null,
+    constraint transaccion_usuario_id_fk
+      foreign key (usuario_id)
+      references usuario(usuario_id),
+    constraint transaccion_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda_venta(vivienda_id),
 );
 
 prompt TABLA SERVICIO
@@ -170,7 +211,14 @@ prompt TABLA VIVIENDA_SERVICIO
 create table vienda_servicio(
     vivienda_servicio_id number(10,0)
       constraint vivienda_servicio_pk primary key,
-
+    servicio_id number(10,0) not null,
+    vivienda_id number(10,0) not null,
+    constraint vs_servicio_id_fk
+      foreign key (servicio_id)
+      references servicio(servicio_id),
+    constraint vs_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda(vivienda_id),
 );
 
 prompt TABLA STATUS_VIVIENDA
@@ -179,6 +227,7 @@ create table status_vivienda(
       constraint status_vivienda_pk primary key,
     clave varchar2(30) not null,
     descripcion varchar2(2000) not null
+
 );
 
 prompt TABLA HISTORICO_VIVIENDA
@@ -186,6 +235,14 @@ create table historico_vivienda(
     historico_vivienda_id number(10,0)
       constraint historico_vivienda_pk primary key,
     fecha_status date not null
+    vivienda_id number(10,0) not null,
+    status_vivienda_id number(10,0) not null,
+    constraint hv_status_vivienda_id_fk
+      foreign key (status_vivienda_id)
+      references status_vivienda(usuario_id),
+    constraint hv_vivienda_id_fk
+      foreign key (vivienda_id)
+      references vivienda(vivienda_id),
 );
 
 prompt TABLA MENSUALIDAD
@@ -196,6 +253,9 @@ create table mensualidad(
     importe number(24,4) not null,
     evidencia blob not null,
     constraint mensualidad_pk primary key(transaccion_id, numero_pago),
+    constraint mensualidad_transaccion_id_fk
+      foreign key (transaccion_id)
+      references transaccion(transaccion_id),
 );
 
 prompt ¡LISTO!
